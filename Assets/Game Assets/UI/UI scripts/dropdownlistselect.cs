5339266511dae875ae8ccd6ff4a08486808b3ac8
oamrtoom <101459192+oamrtoom@users.noreply.github.com>
2023-12-01 22:55:30 +0300

    1/2 Ahmad's editing 😒

--- Assets/Game Assets/UI/UI scripts/dropdownlistselect.cs
+++ Assets/Game Assets/UI/UI scripts/dropdownlistselect.cs
@@ -6,7 +6,7 @@ public class dropdownlistselect : MonoBehaviour
 {
     [SerializeField]private GameObject G;
     [SerializeField]private GameObject G1;
-
+    [SerializeField] private GameObject G2;
     public void select(int index)
     {
         switch (index)
@@ -18,14 +18,17 @@ public class dropdownlistselect : MonoBehaviour
             case 1:
                 G.SetActive(true);
                 G1.SetActive(false);
+                G2.SetActive(false);
                 break;
             case 2:
                 G.SetActive(false);
                 G1.SetActive(true);
+                G2.SetActive(false);
                 break;
             case 3:
                 G.SetActive(false);
                 G1.SetActive(false);
+                G2.SetActive(false);
                 break;
             default:
                 break;
