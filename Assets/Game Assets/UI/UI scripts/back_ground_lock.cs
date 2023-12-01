cca2891c8b5dc986ec3cbe4b8b51fce08c383087
oamrtoom <101459192+oamrtoom@users.noreply.github.com>
2023-12-01 15:24:02 +0300

    first editing in car shop

--- Assets/Game Assets/UI/UI scripts/back_ground_lock.cs
+++ Assets/Game Assets/UI/UI scripts/back_ground_lock.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class back_ground_lock : MonoBehaviour
 {
-    //[SerializeField]
-    //private GameObject G;
-    [SerializeField] private Animator G;
-    void stop(bool l)
+    [SerializeField]
+    private GameObject G;
+    //[SerializeField] private Animator G;
+    void stop()
     {
         //G.GetComponent<Animator>().enabled = false;
-        G.StopPlayback();
+        //G.StopPlayback();
+        // error // G.GetComponent<Animator>().Stop();
+        G.GetComponent<Animator>().StopPlayback();
     }
 
     
