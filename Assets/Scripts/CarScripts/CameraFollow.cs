45dbad9a934c7c6967ab2de1d4364104b7e8ee11
AHMAD OMAR ABUSHARIAH <ahmad16omar77@gmail.com>
2023-12-04 19:18:36 +0300

    اوكي ولا مش اوكي

--- Assets/Scripts/CarScripts/CameraFollow.cs
+++ Assets/Scripts/CarScripts/CameraFollow.cs
@@ -21,12 +21,10 @@ public class CameraFollow : MonoBehaviour {
 
 	void FixedUpdate()
 	{
-		//Look at car
 		Vector3 _lookDirection = (new Vector3(carTransform.position.x, carTransform.position.y, carTransform.position.z)) - transform.position;
 		Quaternion _rot = Quaternion.LookRotation(_lookDirection, Vector3.up);
 		transform.rotation = Quaternion.Lerp(transform.rotation, _rot, lookSpeed * Time.deltaTime);
 
-		//Move to car
 		Vector3 _targetPos = absoluteInitCameraPosition + carTransform.transform.position;
 		transform.position = Vector3.Lerp(transform.position, _targetPos, followSpeed * Time.deltaTime);
 
