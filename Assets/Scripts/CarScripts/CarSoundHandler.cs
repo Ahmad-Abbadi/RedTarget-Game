656fe90a88059eef9db571ea75f4834d96234a2f
AHMAD OMAR ABUSHARIAH <ahmad16omar77@gmail.com>
2023-12-22 22:14:05 +0300

    DataBase Integration 1.1

--- Assets/Scripts/CarScripts/CarSoundHandler.cs
+++ Assets/Scripts/CarScripts/CarSoundHandler.cs
@@ -4,39 +4,58 @@ using UnityEngine;
 
 public class CarSoundHandler : MonoBehaviour
 {
-
-    public AudioSource carEngineSound; // This variable stores the sound of the car engine.
-    public AudioSource tireScreechSound; // This variable stores the sound of the tire screech (when the car is drifting).
-    float initialCarEngineSoundPitch; // Used to store the initial pitch of the car engine sound.
+    public AudioSource carEngineSound;
+    public AudioSource tireScreechSound;
+    float initialCarEngineSoundPitch;
 
     CarController Car;
+
     void Start()
     {
         Car = GetComponent<CarController>();
-        initialCarEngineSoundPitch = carEngineSound.pitch;
+
+       
+            initialCarEngineSoundPitch = carEngineSound.pitch;
+       
     }
 
     void Update()
     {
-        CarSound();
+        if (Car != null)
+        {
+            CarSound();
+        }
+        else
+        {
+            Debug.LogError("Car is null. Make sure the CarController component is present on the GameObject.");
+        }
     }
-
-
 
     public void CarSound()
     {
-        
-          if(carEngineSound != null){
+        if (carEngineSound != null)
+        {
             float engineSoundPitch = initialCarEngineSoundPitch + (Mathf.Abs(Car.carRigidbody.velocity.magnitude) / 25f);
             carEngineSound.pitch = engineSoundPitch;
-          }
-          if((Car.isDrifting) || (Car.isTractionLocked && Mathf.Abs(Car.carSpeed) > 12f)){
-            if(!tireScreechSound.isPlaying){
-              tireScreechSound.Play();
+        }
+
+        if (tireScreechSound != null)
+        {
+            if (Car.isDrifting || (Car.isTractionLocked && Mathf.Abs(Car.carSpeed) > 12f))
+            {
+                if (!tireScreechSound.isPlaying)
+                {
+                    tireScreechSound.Play();
+                }
             }
-          }else if((!Car.isDrifting) && (!Car.isTractionLocked || Mathf.Abs(Car.carSpeed) < 12f)){
-            tireScreechSound.Stop();
-          }
-        
-      }
+            else if (!Car.isDrifting && (!Car.isTractionLocked || Mathf.Abs(Car.carSpeed) < 12f))
+            {
+                tireScreechSound.Stop();
+            }
+        }
+        else
+        {
+            Debug.LogError("tireScreechSound is null. Make sure it's assigned in the inspector.");
+        }
+    }
 }
